f8a47ddc7611f1fbc2543976544f8323665b01cb
Dan Knorr <Daniel.Knorr@gmail.com>
2025-06-20 12:52:34 -0400

    Fixed syntax error in 'add_quarterly_breakdown'.

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ def add_quarterly_breakdown(
         df_func: pd.DataFrame,
         fig_func,
         num_start_strokes: int = 5,
-        num_high_strokes: int = 5
+        num_high_strokes: int = 5,
         show_starting_strokes: bool = False):
     high_strokes_first, *_, high_strokes_last = df_func.loc[
         (df_func.total_strokes > num_start_strokes) & (df_func.total_strokes < num_start_strokes + num_high_strokes), :
