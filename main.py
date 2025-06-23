bc3f346e35f31d135d7d88c4e3e5707fb86f0352
Dan Knorr <Daniel.Knorr@gmail.com>
2025-06-23 11:07:17 -0400

    Trying out not setting x-axis range for 'create_line_plot_speed_stroke_rate()'.

--- main.py
+++ main.py
@@ -227,7 +227,7 @@ def create_line_plot_speed_stroke_rate(
         all_figs = add_quarterly_breakdown(df.copy(), all_figs)
 
     # all_figs.update_xaxes(range=[0, 1000])
-    all_figs.update_xaxes(range=[0, df_func.distance_gps.max()])
+    # all_figs.update_xaxes(range=[0, df_func.distance_gps.max()])
     return all_figs
 
 def create_scatter_plot_speed_colored_stroke_rate(
